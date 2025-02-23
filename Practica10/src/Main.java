2a773ee5dc511681dab88b93c5c78f9cb4646f8f
AxelHm257 <123045383@upq.edu.mx>
2025-02-22 22:27:59 -0600

    Generador de Contraseñas

---
+++ Practica10/src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Passwords primera=new Passwords();
+        int largo =8;
+        String entradai=JOptionPane.showInputDialog("Ingrese la longitud del pasword: ");
+                if (entradai != null && !(entradai.isEmpty()) ){
+            largo=Integer.parseInt(entradai);
+        }
+        String contra= primera.Generador(largo);
+
+        JOptionPane.showMessageDialog(null, "La password es:  "+ contra);
+
+        primera.seguridad(contra);
+
+
+    }
+}
